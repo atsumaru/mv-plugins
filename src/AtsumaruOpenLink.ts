41c223a4399db5093512e0bf21274707f52cd4da
atsumalion <p-rpgtkool-dev@dwango.co.jp>
2019-06-27 12:48:36 +0900

    feat(openlink): 外部リンクAPIの紹介コメント機能

--- src/AtsumaruOpenLink.ts
+++ src/AtsumaruOpenLink.ts
@@ -7,8 +7,12 @@
  * 詳しくはアツマールAPIリファレンス(https://atsumaru.github.io/api-references/popup)を参照してください。
  *
  * プラグインコマンド:
- *   OpenLink <url>         # <url>を開く
+ *   OpenLink <url>         # <url>で外部リンク表示を開く
  *   リンク表示 <url>         # コマンド名が日本語のバージョンです。動作は上記コマンドと同じ
+ *
+ * プラグインコマンド:
+ *   OpenLink <url> <comment>         # <comment>に指定した内容の作者コメントとともに、<url>で外部リンク表示を開く
+ *   リンク表示 <url> <comment>         # コマンド名が日本語のバージョンです。動作は上記コマンドと同じ
  */
 
 import { addPluginCommand, prepareBindPromise } from "./utils/rmmvbridge";
@@ -23,8 +27,8 @@ addPluginCommand({
     "リンク表示": OpenLink
 });
 
-function OpenLink(this: Game_Interpreter, command: string, url?: string) {
+function OpenLink(this: Game_Interpreter, command: string, url?: string, comment?: string) {
     if (openLink) {
-        this.bindPromiseForRPGAtsumaruPlugin(openLink(url!));
+        this.bindPromiseForRPGAtsumaruPlugin(openLink(url!, comment));
     }
 }
